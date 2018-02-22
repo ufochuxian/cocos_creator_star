8e8b94791e24d051da151e9494818b91334caa6a
chenjianxiang <chenjx@chuchujie.com>
2018-02-22 16:14:59 +0800

    second commit

---
+++ assets/scripts/Star.js
@@ -0,0 +1,58 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        pickRadius: 0
+    },
+
+
+    onLoad() {
+
+
+    },
+
+
+    start() {
+
+    },
+
+    // Star.js
+    getPlayerDistance: function () {
+        // 根据 player 节点位置判断距离
+        var playerPos = this.game.player.getPosition();
+        // 根据两点位置计算两点之间距离
+        var dist = cc.pDistance(this.node.position, playerPos);
+        return dist;
+    },
+
+    onPicked: function () {
+        cc.info('onPicked...cc.info')
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+        //调用脚本得分方法
+        this.game.gainScore();
+        // 然后销毁当前星星节点
+        this.node.destroy();
+        cc.info('onPicked end...cc.info')
+    },
+
+    update(dt) {
+        // 每帧判断和主角之间的距离是否小于收集距离
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // 调用收集行为
+            this.onPicked();
+            return;
+        }
+    },
+
+});
---
+++ temp/quick-scripts/assets/scripts/Game.js
@@ -0,0 +1,116 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/scripts/Game.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, 'cb37cJI3zBELJJpmWQTbux5', 'Game', __filename);
+// scripts/Game.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //这个属性引用了星星预制资源
+        starPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+
+        //星星产生后消失时间的随机范围
+        maxStarDuration: 0,
+        minStarDuration: 0,
+
+        //地面节点，用于确定星星生成的高度
+        ground: {
+            default: null,
+            type: cc.Node
+        },
+
+        //player节点，用于获取主角弹跳的高度，和控制主角行动开关
+        player: {
+            default: null,
+            type: cc.Node
+        },
+
+        scoreDisplay: {
+            default: null,
+            type: cc.Label
+        },
+        // 得分音效资源
+        scoreAudio: {
+            default: null,
+            url: cc.AudioClip
+        }
+
+    },
+
+    onLoad: function onLoad() {
+        cc.info('onLoad begin..');
+
+        // 获取地平面的 y 轴坐标
+        this.groundY = this.ground.y + this.ground.height / 2;
+        // 生成一个新的星星
+        this.spawnNewStar();
+
+        this.score = 0;
+
+        cc.info('onLoad end......haha');
+    },
+
+
+    spawnNewStar: function spawnNewStar() {
+        // 使用给定的模板在场景中生成一个新节点
+        var newStar = cc.instantiate(this.starPrefab);
+        // 将新增的节点添加到 Canvas 节点下面
+        this.node.addChild(newStar);
+        // 为星星设置一个随机位置
+        newStar.getComponent('Star').game = this;
+        newStar.setPosition(this.getNewStarPosition());
+    },
+
+    getNewStarPosition: function getNewStarPosition() {
+        var randX = 0;
+        // 根据地平面位置和主角跳跃高度，随机得到一个星星的 y 坐标
+        var randY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight + 50;
+        // 根据屏幕宽度，随机得到一个星星 x 坐标
+        var maxX = this.node.width / 2;
+        randX = cc.randomMinus1To1() * maxX;
+        // 返回星星坐标
+        return cc.p(randX, randY);
+    },
+
+    start: function start() {},
+    update: function update(dt) {},
+
+
+    gainScore: function gainScore() {
+        this.score += 1;
+        // update scoreDisplay Label 文字
+        this.scoreDisplay.string = 'Score:' + this.score.toString();
+
+        //play audio 
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+    }
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=Game.js.map
+        
